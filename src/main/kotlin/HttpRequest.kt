67462a661c2b779590b21c9c72b82524a5b72311
RayGervais <raygervais@hotmail.ca>
2018-11-06 23:56:03 -0500

    update(kotlin): Adds basic GET/calculation route

--- src/main/kotlin/HttpRequest.kt
+++ src/main/kotlin/HttpRequest.kt
@@ -1,9 +1,11 @@
+import com.sun.javafx.scene.control.skin.TableHeaderRow
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.math.BigDecimal
 
 // Following this tutorial: https://ktor.io/quickstart/index.html
 
@@ -15,6 +17,31 @@ fun main(args: Array<String>) {
             }
             get(path="/demo") {
                 call.respondText("<h1>Welcome to KTOR!</h1>", ContentType.Text.Html)
+            }
+            get(path="/calculation/{operation}/") {
+                val operator: String? = call.parameters["operation"]
+                System.out.println(operator)
+
+                val queryParams: Set<Map.Entry<String, List<String>>> = call.request.queryParameters.entries()
+                System.out.println(queryParams.toString())
+
+                val primaryNumeric: BigDecimal = call.request.queryParameters.get("primary")!!.toBigDecimal()
+                val secondaryNumeric: BigDecimal = call.request.queryParameters.get("secondary")!!.toBigDecimal()
+
+                try {
+                    val result: BigDecimal = when (operator) {
+                        "add" -> (primaryNumeric + secondaryNumeric)
+                        "subtract" -> (primaryNumeric - secondaryNumeric)
+                        "divide" -> (primaryNumeric / secondaryNumeric)
+                        "multiply" -> (primaryNumeric * secondaryNumeric)
+                        else -> throw error("Invalid Operator Provided!")
+                    }
+                    call.respondText(result.toString(), ContentType.Text.JavaScript )
+                } catch (e: ArithmeticException) {
+                    call.response.status(HttpStatusCode(500, "Invalid Calculation Attempted"))
+                    call.respondText("Error! Invalid Calculation! <br /> Attempt "  + e.message!!, ContentType.Text.Html)
+                }
+
             }
         }
     }
